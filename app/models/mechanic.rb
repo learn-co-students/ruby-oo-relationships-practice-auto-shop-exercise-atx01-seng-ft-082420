d3e90365b04fa6617f5a094486297820523ff4d9
hproctor021 <hproctor021@gmail.com>
2020-09-02 11:37:26 -0500

    commit changes

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,28 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
+  
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select {|car| car.mechanic == self}
+  end
+
+  def car_owners
+    cars.collect {|car| car.car_owner}
+  end
+
+  def car_owners_names
+    car_owners.collect {|owner| owner.name}
+  end
 
 end
